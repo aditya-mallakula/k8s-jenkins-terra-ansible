c5c41f59f2e9d393707a45e399dc07a3c917498e
aditya-mallakula <amallakula@gmail.com>
2025-09-07 12:51:07 -0500

    fix(terraform): added public access

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -33,6 +33,10 @@ module "eks" {
   subnet_ids      = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = false
+  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]  # or restrict to your IP/32
+
   eks_managed_node_groups = {
     ng1 = {
       instance_types = ["t3.small"]
